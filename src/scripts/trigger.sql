b504d9480c66ae483357bb403b087762bc4c5cd5
Fede Mamone <fmamone@itba.edu.ar>
2018-06-09 19:01:34 -0300

    Improve trigger performance

--- src/scripts/trigger.sql
+++ src/scripts/trigger.sql
@@ -3,39 +3,19 @@ CREATE OR REPLACE FUNCTION verifica_solapado()
 RETURNS trigger AS $$
 DECLARE
 
-usu recorrido_final.usuario%TYPE;
-fecha_ret recorrido_final.fecha_hora_ret%TYPE;
-fecha_dev recorrido_final.fecha_hora_dev%TYPE;
-
-usuarioCursor CURSOR FOR
- SELECT usuario FROM recorrido_final;
-fechaCursor CURSOR FOR
- SELECT fecha_hora_ret FROM recorrido_final where usuario = usu;
+encontrado int;
 
 BEGIN
-OPEN usuarioCursor;
-LOOP
-        FETCH usuarioCursor INTO usu;
 
-        OPEN fechaCursor;
-        LOOP
-            
-            FETCH fechaCursor INTO fecha_ret;
-            SELECT fecha_hora_dev INTO fecha_dev FROM recorrido_final WHERE usuario = usu AND fecha_hora_ret = fecha_ret;
-
-            IF NEW.usuario = usu THEN
-                IF NEW.fecha_hora_ret BETWEEN fecha_ret AND fecha_dev THEN
-                    raise EXCEPTION 'INSERCION IMPOSIBLE POR SOLAPAMIENTO';
-                END IF;    
-            END IF;    
-
-            EXIT WHEN NOT FOUND;
-        END LOOP;
-        CLOSE fechaCursor;
+        SELECT COUNT(*) INTO encontrado 
+        FROM recorrido_final 
+        WHERE usuario = NEW.usuario 
+        AND NEW.fecha_hora_ret BETWEEN fecha_hora_ret AND fecha_hora_dev;
         
-        EXIT WHEN NOT FOUND;
-END LOOP;
-CLOSE usuarioCursor;
+        IF encontrado > 0 THEN
+                raise EXCEPTION 'INSERCION IMPOSIBLE POR SOLAPAMIENTO';
+        END IF;
+
 RETURN NEW;
 END;
 $$ LANGUAGE plpgsql; 
